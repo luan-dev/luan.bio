a722cd45c16e306cbdb5baa311fa088b2d76c36c
luan-dev <progluan@gmail.com>
2024-09-07 00:36:00 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import logo from './assets/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={logo} className="logo" alt="Luan logo" />
+      </div>
+      <h1>Welcome</h1>
+    </>
+  );
+}
+
+export default App;
